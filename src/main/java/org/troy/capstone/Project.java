01594434a8818a1c15dc4e6ff767b9fec57ea062
tkleg <thkleger@gmail.com>
2026-01-29 14:28:30 -0500

    can run with tablesaw library

--- src/main/java/org/troy/capstone/Project.java
+++ src/main/java/org/troy/capstone/Project.java
@@ -4,13 +4,31 @@
 
 package org.troy.capstone;
 
+import tech.tablesaw.api.DoubleColumn;
+import tech.tablesaw.api.StringColumn;
+import tech.tablesaw.api.Table;
+
 /**
  *
  * @author thkle
  */
 public class Project {
 
+    static {
+        // Suppress SLF4J warnings
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "off");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String[] animals = {"bear", "cat", "giraffe"};
+        double[] cuteness = {90.1, 84.3, 99.7};
+
+        Table cuteAnimals =
+            Table.create("Cute Animals")
+                .addColumns(
+                    StringColumn.create("Animal types", animals),
+                    DoubleColumn.create("rating", cuteness));
+
+        System.out.println(cuteAnimals.print());
     }
 }
