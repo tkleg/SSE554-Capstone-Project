5d6db92edbcd13fc6236199a657068e3373b5b13
tkleg <thkleger@gmail.com>
2026-01-29 16:01:30 -0500

    javaFXTest can launch

---
+++ src/main/java/org/troy/capstone/JavaFXTest.java
@@ -0,0 +1,18 @@
+package org.troy.capstone;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXTest extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Test");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
