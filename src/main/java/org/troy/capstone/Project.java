807dcc9ce88e67784474606ad5d53b0edd2df35d
tkleg <thkleger@gmail.com>
2026-01-29 14:02:26 -0500

    maven project initialized

---
+++ src/main/java/org/troy/capstone/Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.troy.capstone;
+
+/**
+ *
+ * @author thkle
+ */
+public class Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
