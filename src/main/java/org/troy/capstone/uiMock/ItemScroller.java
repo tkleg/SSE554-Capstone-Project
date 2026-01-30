6cc8fe026579b227a04c38a0c8f1f5035a1498c0
tkleg <thkleger@gmail.com>
2026-01-29 19:51:15 -0500

    itemScroller object made

---
+++ src/main/java/org/troy/capstone/uiMock/ItemScroller.java
@@ -0,0 +1,24 @@
+package org.troy.capstone.uiMock;
+
+import java.util.List;
+
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class ItemScroller extends ScrollPane {
+    private VBox itemContainer;
+    
+    public ItemScroller() {
+        super();
+        itemContainer = new VBox(5); // 5px spacing between items
+        setContent(itemContainer);
+    }
+
+    public void addItemPanel(ItemPanel itemPanel) {
+        itemContainer.getChildren().add(itemPanel);
+    }
+
+    public void addItemPanels(List<ItemPanel> itemPanels) {
+        itemContainer.getChildren().addAll(itemPanels);
+    }
+}
