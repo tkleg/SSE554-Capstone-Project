b6e2f3bbfba0a4525c05e9cfc232020361dc4e4a
tkleg <thkleger@gmail.com>
2026-01-30 21:33:35 -0500

    generalized the CategoryPanel to a FilterPanel

---
+++ src/main/java/org/troy/capstone/uiMock/FilterPanel.java
@@ -0,0 +1,38 @@
+package org.troy.capstone.uiMock;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class FilterPanel extends VBox{
+    private final Set<CheckBox> optionCheckBoxes;//Not the same as the tile pane that holds them
+    //This list is meant to make it easier to retrieve which options are selected
+
+    public FilterPanel(String title, Set<String> options) {
+        optionCheckBoxes = new HashSet<>();
+        setAlignment(Pos.CENTER_LEFT); // Align everything to the left
+        
+        //Add header label first (at the top)
+        Label titleLabel = new Label(title);
+        titleLabel.setStyle("-fx-font-weight: bold;");
+        getChildren().add(titleLabel);
+        
+        for(String option : options) {
+            CheckBox optionBox = new CheckBox(option);
+            getChildren().add(optionBox);
+            optionCheckBoxes.add(optionBox);
+        }
+    }
+
+    public Set<String> getCheckedOptions() {
+        return optionCheckBoxes.stream()
+            .filter(CheckBox::isSelected)
+            .map(CheckBox::getText)
+            .collect(Collectors.toSet());
+    }
+}
