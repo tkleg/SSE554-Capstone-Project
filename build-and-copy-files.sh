ffb73fccb4562bc5bad9ca023484f533979226ef
tkleg <thkleger@gmail.com>
2026-02-16 19:37:13 -0500

    fixed issue wit build-and-copy-files.sh that casued data to not be copied into the distribution folder

--- build-and-copy-files.sh
+++ build-and-copy-files.sh
@@ -6,4 +6,4 @@ mvn clean package
 # Copy necessary files to distribution directory
 cp target/Project-1.0-SNAPSHOT-with-dependencies.jar distribution
 cp -r target/dependency distribution
-cp -r data target/data
+cp -r data distribution
